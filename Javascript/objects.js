d855c604eb714a0220300b6e9896399182985ee6
Emma <ramirezerick15@gmail.com>
2015-12-22 06:03:04 -0500

    objects

---
+++ Javascript/objects.js
@@ -0,0 +1,12 @@
+for (var key in superman) {
+  if (superman.hasOwnProperty(key)) {
+    console.log(key + ": " + superman[key]);
+  }
+}
+
+delete Superman.fly
+
+// if you use a falsey value, like Zero
+// then you
+
+today = new Date();
