bd2a9455dd23decc1d45037d5e77b3db10a962f0
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-03-04 17:36:52 -0700

    move/delete most files

--- Haskell/Notes/Getting-started/triangles.hs
+++
@@ -1,6 +0,0 @@
-let triangles = [ (a, b, c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
--- draw from three lists and combine them into a triple
-let rightTriangles = [ (a, b, c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
--- returns a list where the numbers form right triangles
-let rightTriangles' = [ (a, b, c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a + b + c == 24]
--- returns [(6, 8, 10)]
