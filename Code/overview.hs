df0c95d09b9e0c5e98bb3ad7eef075da0fff2f02
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-03-17 06:14:21 -0600

    add section on functions

--- Code/overview.hs
+++ Code/overview.hs
@@ -78,3 +78,53 @@ pow3List = [3^n | n <- [1..10]]
 pow3ListDiv9 = [3^n | n <- [1..10], 3^n mod 9 == 0]
 
 multTable = [[x * y | y <- [1..10]] | x <- [1..10]]
+
+-- Functions --
+
+addTuples :: (Int, Int) -> (Int, Int) -> (Int, Int)
+addTuples (x, y) (x2, yx) = (x + x2, y + y2)
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+productFactorial n = product [1..n]
+
+isOdd :: Int -> Bool
+isOdd n
+    | n mod 2 == 0 = False
+    | otherwise = True
+
+isEven n = n mod 2 == 0
+
+whatGrade :: Int -> String
+whatGrade age
+	| (age >= 5) && (age <= 6) = "Kindergarten"
+	| (age > 6) && (age <= 10) = "Elementary School"
+	| (age > 10) && (age <= 14) = "Middle School"
+	| (age > 14) && (age <= 18) = "High School"
+	| otherwise = "Go to college"
+
+
+batAvgRating :: Double -> Double -> String
+batAvgRating hits atBats
+	| avg <= 0.200 = "Terrible Batting Average"
+	| avg <= 0.250 = "Average Player"
+	| avg <= 0.280 = "Your doing pretty good"
+	| otherwise = "You're a Superstar"
+	where avg = hits / atBats
+
+-- You can access list items by separating letters with : or get everything but
+-- the first item with xs
+getListItems :: [Int] -> String
+getListItems [] = "Your list is empty"
+getListItems (x:[]) = "Your list contains " ++ show x
+getListItems (x:y:[]) = "Your list contains " ++ show x ++ " and " ++ show y
+getListItems (x:xs) = "The first item is " ++ show x ++ " and the rest are "
+	++ show xs
+
+-- We can also get values with an As pattern
+getFirstItem :: String -> String
+getFirstItem [] = "Empty String"
+getFirstItem all@(x:xs) = "The first letter in " ++ all ++ " is "
+	++ [x]
